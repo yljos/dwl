eilefsen/dotify

commit 71f11e6cf63289d51f152469a0da81a85fe2608c

set O_NONBLOCK flag to stdout

util.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 #include "util.h"
 
@@ -32,4 +33,19 @@ ecalloc(size_t nmemb, size_t size)
 	if (!(p = calloc(nmemb, size)))
 		die("calloc:");
 	return p;
+}
+
+int
+fd_set_nonblock(int fd) {
+	int flags = fcntl(fd, F_GETFL);
+    if (flags < 0) {
+		perror("fcntl(F_GETFL):");
+        return -1;
+    }
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+		perror("fcntl(F_SETFL):");
+		return -1;
+    }
+
+	return 0;
 }

dwl.c
@@ -2190,6 +2190,12 @@ run(char *startup_cmd)
 		close(piperw[1]);
 		close(piperw[0]);
 	}
+
+	/* Mark stdout as non-blocking to avoid people who does not close stdin
+	 * nor consumes it in their startup script getting dwl frozen */
+	if (fd_set_nonblock(STDOUT_FILENO) < 0)
+		close(STDOUT_FILENO);
+
 	printstatus();
 
 	/* At this point the outputs are initialized, choose initial selmon based on

util.h
@@ -2,3 +2,4 @@
 
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
+int fd_set_nonblock(int fd);

yljos/dwl

commit 986beef5be32a2bead22ca85d19f4d6d7e5c0ce1

replace spaces with tabs
Fixes: 71f11e6cf63289d51f152469a0da81a85fe2608c

util.c
@@ -38,14 +38,14 @@ ecalloc(size_t nmemb, size_t size)
 int
 fd_set_nonblock(int fd) {
 	int flags = fcntl(fd, F_GETFL);
-    if (flags < 0) {
+	if (flags < 0) {
 		perror("fcntl(F_GETFL):");
-        return -1;
-    }
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+		return -1;
+	}
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
 		perror("fcntl(F_SETFL):");
 		return -1;
-    }
+	}
 
 	return 0;
 }