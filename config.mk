d136dadf456250f9a4cef23abd1fdf21518a31ba
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-08-01 20:38:57 -0600

    `-pedantic` -> `-Wpedantic`
    Bug: https://codeberg.org/dwl/dwl/issues/584

--- config.mk
+++ config.mk
@@ -14,4 +14,7 @@ XLIBS =
 #XWAYLAND = -DXWAYLAND
 #XLIBS = xcb xcb-icccm
 
-CC = gcc
+# dwl itself only uses C99 features, but wlroots' headers use anonymous unions (C11).
+# To avoid warnings about them, we do not use -std=c99 and instead of using the
+# gmake default 'CC=c99', we use cc.
+CC = cc
