5db05e82bd7d1f6bf3cb0ce1ddd07952f06e57f8
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-08-09 20:51:33 -0600

    fix style in configurex11()

--- dwl.c
+++ dwl.c
@@ -3051,11 +3051,13 @@ configurex11(struct wl_listener *listener, void *data)
 				event->x, event->y, event->width, event->height);
 		return;
 	}
-	if ((c->isfloating && c != grabc) || !c->mon->lt[c->mon->sellt]->arrange)
-		resize(c, (struct wlr_box){.x = event->x - c->bw, .y = event->y - c->bw,
-				.width = event->width + c->bw * 2, .height = event->height + c->bw * 2}, 0);
-	else
+	if ((c->isfloating && c != grabc) || !c->mon->lt[c->mon->sellt]->arrange) {
+		resize(c, (struct wlr_box){.x = event->x - c->bw,
+				.y = event->y - c->bw, .width = event->width + c->bw * 2,
+				.height = event->height + c->bw * 2}, 0);
+	} else {
 		arrange(c->mon);
+	}
 }
 
 void
