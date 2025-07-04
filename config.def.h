e7ee7ed953a1ee51b91c2029a85f3460436f7fb1
yljos <huai12138@hotmail.com>
2025-07-04 10:43:34 +0800

    color

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You ca
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
 	[SchemeNorm] = { 0xbbbbbbff, 0x222222ff, 0x444444ff },
-	[SchemeSel]  = { 0xeeeeeeff, 0x005577ff, 0x005577ff },
+	[SchemeSel]  = { 0x00ff00ff, 0x222222ff, 0x005577ff },
 	[SchemeUrg]  = { 0,          0,          0x770000ff },
 };
 
