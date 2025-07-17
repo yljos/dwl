072c0bc1d5b0c44ed03c9b045642605720c2ba20
yljos <huai12138@hotmail.com>
2025-07-17 19:44:34 +0800

    fictx5

--- config.def.h
+++ config.def.h
@@ -136,13 +136,14 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
-
+static const char *menucmd[] = { "fuzzel", NULL };
+static const char *fcitx5cmd[] = { "fcitx5-remote", "-t", NULL };
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
+	{ WLR_MODIFIER_CTRL,         XKB_KEY_space,      spawn,          {.v = fcitx5cmd} },
 	{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
