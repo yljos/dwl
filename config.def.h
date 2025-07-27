c3ffb5b646ab062c741a280da510f7685cca4830
yljos <huai12138@hotmail.com>
2025-07-27 10:30:48 +0800

    firefox 1

--- config.def.h
+++ config.def.h
@@ -40,8 +40,8 @@ static const char *const autostart[] = {
 static const Rule rules[] = {
 	/* app_id             title       tags mask     isfloating   monitor */
 	/* examples: */
-	{ "Gimp_EXAMPLE",     NULL,       0,            1,           -1 }, /* Start on currently visible tags floating, not tiled */
-	{ "firefox_EXAMPLE",  NULL,       1 << 8,       0,           -1 }, /* Start on ONLY tag "9" */
+	{ "Gimp_EXAMPLE",        NULL,       0,            1,           -1 }, /* Start on currently visible tags floating, not tiled */
+	{ "firefox",             NULL,       1 << 0,       0,           -1 }, /* Start on ONLY tag "9" */
 	{ "zen",                 NULL,       1 << 0,       0,           -1 },
 	{ "chromium",            NULL,       1 << 0,       0,           -1 },
 	{ "telegram",            NULL,       1 << 1,       0,           -1 },
