3640a99beb6516775d9453a0603967870d98bf5c
bitarc <yljos@outlook.com>
2025-09-05 09:58:01 +0800

    fix

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static uint32_t colors[][3]                = {
 };
 
 /* tagging - TAGCOUNT must be no greater than 31 */
-static char *tags[] = {"", "", "󰇄", "", "󰝚", "", "", "8", "9" };
+static char *tags[] = {"", "", "󰇄", "", "󰝚", "", ""};
 
 /* logging */
 static int log_level = WLR_ERROR;
