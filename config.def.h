db8c4c878ca05034795fb65bbaaa330e9ee56fe4
yljos <yljos@outlook.com>
2025-09-18 16:59:06 +0800

    icon

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static uint32_t colors[][3]                = {
 };
 
 /* tagging - TAGCOUNT must be no greater than 31 */
-static char *tags[] = {"", "", "󰇄", "", "󰝚", "", ""};
+static char *tags[] = {"Firefox", "Telegram", "Windows", "Foot", "Music", "Steam", "Game"};
 
 /* logging */
 static int log_level = WLR_ERROR;
