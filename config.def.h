b2e6bf83aab58580fdef423f9ee736a33adda037
yljos <yljos@outlook.com>
2025-11-03 12:54:17 +0800

    py

--- config.def.h
+++ config.def.h
@@ -147,7 +147,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
-static const char *fcitx5cmd[] = { "/home/huai/.config/dwl/ime.sh", NULL };
+static const char *fcitx5cmd[] = { "python3","/home/huai/.config/dwl/notify_ime.py", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *minecraftcmd[] = { "prismlauncher", NULL };
 static const Key keys[] = {
