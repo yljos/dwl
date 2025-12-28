57309d17899f53cf78eaf0e8acdf6605fa517f4e
yljos <yljos@outlook.com>
2025-12-28 14:42:53 +0800

    fuzzel

--- config.def.h
+++ config.def.h
@@ -146,7 +146,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
+static const char *menucmd[] = { "fuzzel", NULL };
 static const char *fcitx5cmd[] = { "/usr/bin/sh","/home/huai/.config/dwl/ime.sh", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *minecraftcmd[] = { "prismlauncher", NULL };
